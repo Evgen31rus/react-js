c58c4e8d927e99f99cd44bafe2fa11deedbff19a
Evgen31rus <boom.boom.1998@list.ru>
2023-05-31 12:17:49 +0300

    https://github.com/Evgen31rus/react-js.git

---
+++ src/Components/ShopItemFunc.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './main.css';
+import ShopItemClass from './ShopItemClass/ShopItemClass.jsx';
+
+export const ShopItemFunc = () => {
+  return (
+    <ShopItemClass/>
+  )
+}
